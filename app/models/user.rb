f795cfebea79157dad9e9403b9b17e53ba4137bf
Ryan Kretch <rkretch91@gmail.com>
2018-03-08 20:04:21 -0300

    events model has validations, join table created between events and users models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
+  has_and_belongs_to_many :events, join_table: "users_events"
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
